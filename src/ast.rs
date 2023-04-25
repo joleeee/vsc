0849e5fa59543347547528ebf17582a7c5dd48b8
Jonas <jonas@jole.xyz>
2023-04-25 14:30:09 +0200

    intermediary

--- src/ast.rs
+++ src/ast.rs
@@ -4,30 +4,43 @@ use std::collections::VecDeque;
 pub use types::*;
 
 // parse identifier or identifier with data
-pub fn parse_identifier(input: &str) -> (String, Option<String>) {
+pub fn parse_identifier(input: &str) -> Field {
     let parts = input.split('(').collect::<Vec<_>>();
     // SOME_IDENTIFIER(2)
     // -> ["SOME_IDENTIFIER", "2)"]
 
     let name = parts[0].to_string();
 
-    if let Some(data) = parts.get(1) {
-        let data = data.chars().take_while(|&x| x != ')').collect::<String>();
-        (name, Some(data))
-    } else {
-        (name, None)
-    }
+    let argument = parts
+        .get(1)
+        .and_then(|data| Some(data.chars().take_while(|&x| x != ')').collect::<String>()));
+
+    Field { name, argument }
 }
 
-pub fn parse() {
-    let symbols = std::fs::read_to_string("locals.symbols").unwrap();
+#[derive(Debug, Clone)]
+pub struct Entry(Vec<Field>);
 
-    let ast = symbols.split(" == BOUND SYNTAX TREE == \n").last().unwrap();
-    let ast = ast.split("\n").map(|x| x.trim_end()).collect::<Vec<&str>>();
+#[derive(Debug, Clone)]
+struct RawSymbol {
+    indent: usize,
+    objects: Vec<Field>,
+}
 
-    let mut symbols = VecDeque::new();
+#[derive(Debug, Clone)]
+pub struct Field {
+    name: String,
+    argument: Option<String>,
+}
 
-    for line in ast {
+fn parse_raw_symbols(lines: Vec<&str>) -> Vec<RawSymbol> {
+    let mut symbols = Vec::new();
+
+    for line in lines {
+        if line.len() == 0 {
+            continue;
+        }
+
         let indent = line.chars().take_while(|x| *x == ' ').count();
 
         let remaining = line.chars().skip(indent).collect::<String>();
@@ -38,45 +51,96 @@ pub fn parse() {
             .map(parse_identifier)
             .collect::<Vec<_>>();
 
-        symbols.push_back(Parent { indent, objects })
+        symbols.push(RawSymbol { indent, objects })
     }
+
+    symbols
+}
+
+pub fn parse() {
+    let symbols = std::fs::read_to_string("locals.symbols").unwrap();
+
+    let ast = symbols.split(" == BOUND SYNTAX TREE == \n").last().unwrap();
+    let ast = ast.split("\n").map(|x| x.trim_end()).collect::<Vec<&str>>();
+
+    let symbols = parse_raw_symbols(ast);
+
+    // add one 0-level at the end to pop everything
+    let mut symbols = symbols;
+    symbols.push(RawSymbol {
+        indent: 0,
+        objects: vec![],
+    });
+    let symbols = symbols;
 
     #[derive(Debug)]
-    struct Parent {
-        indent: usize,
-        objects: Vec<(String, Option<String>)>,
+    struct IndexedRawSymbol<'a> {
+        idx: usize,
+        sym: &'a RawSymbol,
     }
 
-    let mut parents: VecDeque<&Parent> = VecDeque::new();
+    let mut callstack: VecDeque<IndexedRawSymbol> = VecDeque::new();
 
-    let global_list = symbols.pop_front().unwrap();
-    dbg!(&global_list);
-    assert_eq!(global_list.objects[0].0, "GLOBAL_LIST");
-    parents.push_back(&global_list);
+    let global_list = &symbols[0];
+    assert_eq!(global_list.objects[0].name, "GLOBAL_LIST");
+    callstack.push_back(IndexedRawSymbol {
+        idx: 0,
+        sym: &global_list,
+    });
 
-    for sym in &symbols {
-        while sym.indent < parents.back().unwrap().indent {
+    let mut graph = vec![vec![]; symbols.len()];
+
+    for (idx, sym) in symbols.iter().enumerate().skip(1) {
+        while sym.indent < callstack.back().unwrap().sym.indent {
             let mut arguments = vec![];
 
             // we are at a lower indentation level, pop parents until we are at the same level
-            let target_indentation = parents.back().unwrap().indent - 1;
+            let target_indentation = callstack.back().unwrap().sym.indent - 1;
 
-            while target_indentation < parents.back().unwrap().indent {
-                arguments.push(parents.pop_back().unwrap());
+            while target_indentation < callstack.back().unwrap().sym.indent {
+                arguments.push(callstack.pop_back().unwrap());
             }
 
-            println!(
-                "> Calling {} with {} arguments",
-                parents.back().unwrap().objects[0].0,
-                arguments.len()
-            );
-            for a in arguments {
-                println!("> > {} {:?}", a.objects[0].0, a.objects[0].1);
-            }
+            let parent = callstack.back().unwrap();
+
+            //println!(
+            //    "> Calling {} (idx {}) with {} arguments",
+            //    parent.sym.objects[0].name,
+            //    parent.idx,
+            //    arguments.len()
+            //);
+            //for a in &arguments {
+            //    println!(
+            //        "> > {} {:?}",
+            //        a.sym.objects[0].name, a.sym.objects[0].argument
+            //    );
+            //}
+
+            graph[parent.idx].extend(arguments.iter().rev().map(|a| a.idx));
         }
 
-        parents.push_back(sym);
+        callstack.push_back(IndexedRawSymbol { idx, sym });
 
-        println!("{}{:?}", " ".repeat(sym.indent), sym.objects);
+        //println!("{}{:?}", " ".repeat(sym.indent), sym.objects);
     }
+
+    assert_eq!(callstack.len(), 2); // GLOBAL_LIST, and the dummy one
+    
+    // stuff
+    
+    generate(&symbols, &graph, 0);
+}
+
+fn generate(syms: &Vec<RawSymbol>, graph: &Vec<Vec<usize>>, idx: usize) -> Node {
+    // first generate any children
+    let mut children = Vec::new();
+    for ci in &graph[idx] {
+        let ch = generate(syms, graph, *ci);
+        children.push(ch);
+    }
+    
+    // then generate this one
+    let node = generate_node_good(Entry(syms[idx].objects.clone()), &children);
+    
+    node
 }
--- src/ast/types.rs
+++ src/ast/types.rs
@@ -1,14 +1,20 @@
+use super::Field;
+
+#[derive(Debug, Clone)]
 struct Identifier {
     name: String,
 }
 
+#[derive(Debug, Clone)]
 struct LocatedIdentifier {
     name: Identifier,
     location: Location,
 }
 
+#[derive(Debug, Clone)]
 struct Parameter(Identifier);
 
+#[derive(Debug, Clone)]
 struct Function {
     name: Identifier,
     parameters: Vec<Parameter>,
@@ -17,31 +23,57 @@ struct Function {
     return_statement: Block,
 }
 
+#[derive(Debug, Clone)]
 struct Block {
     children: Vec<BlockChild>,
 }
 
+#[derive(Debug, Clone)]
 enum BlockChild {
     StatementList(Vec<Statement>),
     DeclarationList(Vec<Declaration>),
     ReturnStatement,
 }
 
+#[derive(Debug, Clone)]
 struct Declaration {
     names: Vec<Identifier>,
 }
 
+#[derive(Debug, Clone)]
 enum Expression {
     Variable(LocatedIdentifier),
+    Constant(i32),
 
     // operations
-    Plus(Box<Expression>, Box<Expression>),
+    Add(Box<Expression>, Box<Expression>),
     Minus(Box<Expression>, Box<Expression>),
     Multiply(Box<Expression>, Box<Expression>),
     Divide(Box<Expression>, Box<Expression>),
     Negative(Box<Expression>),
 }
 
+#[derive(Debug, Clone)]
+pub enum Node {
+    Function(Function),
+    Block(Block),
+    ParameterList(Vec<Parameter>),
+    Expression(Expression),
+
+    Declaration(Declaration),
+    DeclarationList(Vec<Declaration>),
+    AssignmentStatement(AssignmentStatement),
+    PrintStatement(PrintStatement),
+
+    Identifier(Identifier),
+    LocatedIdentifier(LocatedIdentifier),
+    NumberData(i32),
+
+    Relation(Relation),
+    IfStatement(IfStatement),
+}
+
+#[derive(Debug, Clone)]
 enum Statement {
     Assignment(AssignmentStatement),
     Print(PrintStatement),
@@ -49,25 +81,186 @@ enum Statement {
     Block(Block),
 }
 
+#[derive(Debug, Clone)]
 struct PrintStatement {
-    args: Vec<(Identifier, Location)>,
+    //args: Vec<(Identifier, Location)>,
+    args: Vec<Node>,
 }
 
+#[derive(Debug, Clone)]
 struct IfStatement {
     condition: Relation,
     statement: Box<Statement>,
 }
 
+#[derive(Debug, Clone)]
 enum Location {
     Parameter(i32),
     Local(i32),
 }
 
+#[derive(Debug, Clone)]
 struct Relation {
-    // todo
+    left: Expression,
+    right: Expression,
+    operator: char, // TODO
 }
 
+#[derive(Debug, Clone)]
 struct AssignmentStatement {
     left: LocatedIdentifier,
     right: Expression,
+}
+
+pub fn generate_node_good(e: super::Entry, args: &Vec<Node>) -> Node {
+    let Field {
+        name,
+        argument: innerarg,
+    } = &e.0[0];
+
+    return match name.as_str() {
+        "BLOCK" => Node::Block(Block { children: vec![] }),
+        "PARAMETER_LIST" => {
+            let mut data = vec![];
+
+            for d in args {
+                match d {
+                    Node::Identifier(p) => data.push(Parameter(p.clone())),
+                    _x => panic!("Expected parameter, got {:?}", _x),
+                }
+            }
+
+            Node::ParameterList(data)
+        }
+        "IDENTIFIER_DATA" => {
+            let name = innerarg.as_ref().unwrap().clone();
+            let id = Identifier { name };
+
+            if let Some(loc) = e.0.get(1) {
+                let position = loc.argument.as_ref().unwrap().parse().unwrap();
+                let location = match loc.name.as_str() {
+                    "LOCAL_VAR" => Location::Local(position),
+                    "PARAMETER" => Location::Parameter(position),
+                    _x => panic!("Unknown location, got {}", _x),
+                };
+
+                Node::LocatedIdentifier(LocatedIdentifier { name: id, location })
+            } else {
+                Node::Identifier(id)
+            }
+        }
+        "DECLARATION" => {
+            let mut names = vec![];
+
+            for d in args {
+                match d {
+                    Node::Identifier(p) => names.push(p.clone()),
+                    _x => panic!("Expected parameter, got {:?}", _x),
+                }
+            }
+
+            Node::Declaration(Declaration { names })
+        }
+        "DECLARATION_LIST" => {
+            let mut declarations = vec![];
+
+            for d in args {
+                match d {
+                    Node::Declaration(p) => declarations.push((*p).clone()),
+                    _x => panic!("Expected parameter, got {:?}", _x),
+                }
+            }
+
+            Node::DeclarationList(declarations)
+        }
+        "EXPRESSION" => {
+            fn evaluate(x: &Node) -> Expression {
+                match x {
+                    Node::Expression(p) => p.clone(),
+                    Node::LocatedIdentifier(i) => Expression::Variable(i.clone()),
+                    _x => panic!("Expected expression, got {:?}", _x),
+                }
+            }
+
+            // lazy eval, in case there is only one arg
+            let first = || evaluate(&args[0]);
+            let second = || evaluate(&args[1]);
+
+            Node::Expression(match innerarg.as_ref().unwrap().as_str() {
+                "+" => Expression::Add(Box::new(first()), Box::new(second())),
+                "-" => {
+                    if args.len() == 1 {
+                        Expression::Negative(Box::new(first()))
+                    } else {
+                        Expression::Minus(Box::new(first()), Box::new(second()))
+                    }
+                }
+                "*" => Expression::Multiply(Box::new(first()), Box::new(second())),
+                "/" => Expression::Divide(Box::new(first()), Box::new(second())),
+                _ => todo!(),
+            })
+        }
+        "ASSIGNMENT_STATEMENT" => {
+            let left = match &args[0] {
+                Node::LocatedIdentifier(id) => id.clone(),
+                _x => panic!("Expected identifier, got {:?}", _x),
+            };
+
+            let right = match &args[1] {
+                Node::Expression(p) => p.clone(),
+                Node::LocatedIdentifier(i) => Expression::Variable(i.clone()),
+                Node::NumberData(n) => Expression::Constant(*n),
+                _x => panic!("Expected expression, got {:?}", _x),
+            };
+
+            Node::AssignmentStatement(AssignmentStatement { left, right })
+        }
+        "PRINT_STATEMENT" => Node::PrintStatement(PrintStatement {
+            args: args.to_vec(),
+        }),
+        "NUMBER_DATA" => {
+            let n = innerarg.as_ref().unwrap().parse().unwrap();
+            Node::NumberData(n)
+        }
+        "RELATION" => {
+            let operator = innerarg.as_ref().unwrap().chars().next().unwrap();
+
+            let left = match &args[0] {
+                Node::Expression(p) => p.clone(),
+                Node::LocatedIdentifier(i) => Expression::Variable(i.clone()),
+                Node::NumberData(n) => Expression::Constant(*n),
+                _x => panic!("Expected expression, got {:?}", _x),
+            };
+
+            let right = match &args[1] {
+                Node::Expression(p) => p.clone(),
+                Node::LocatedIdentifier(i) => Expression::Variable(i.clone()),
+                Node::NumberData(n) => Expression::Constant(*n),
+                _x => panic!("Expected expression, got {:?}", _x),
+            };
+
+            Node::Relation(Relation {
+                left,
+                right,
+                operator,
+            })
+        }
+        "IF_STATEMENT" => {
+            let relation = match &args[0] {
+                Node::Relation(rel) => rel.clone(),
+                _x => panic!("Expected relation, got {:?}", _x),
+            };
+
+            let statement = match &args[1] {
+                Node::AssignmentStatement(s) => Statement::Assignment(s.clone()),
+                _x => panic!("Expected statement, got {:?}", _x),
+            };
+
+            Node::IfStatement(IfStatement {
+                condition: relation,
+                statement: Box::new(statement),
+            })
+        }
+        _ => panic!("Unknown type {}", name),
+    };
 }
