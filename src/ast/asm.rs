660f5439f26da7cadcdab8efdc208f300c6eb7b8
Jonas <jonas@jole.xyz>
2023-04-26 15:38:41 +0200

    remove unused imports

--- src/ast/asm.rs
+++ src/ast/asm.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, io::Write};
 
-use super::{Globals, Identifier, Node, ParsedProgram};
+use super::{Globals, ParsedProgram};
 
 #[derive(Debug)]
 struct GlobalSymbol {
