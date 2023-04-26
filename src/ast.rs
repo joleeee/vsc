cdd41a9f1e24b4df30f92a05652d660827490bed
Jonas <jonas@jole.xyz>
2023-04-26 14:00:39 +0200

    cleany clean clean

--- src/ast.rs
+++ src/ast.rs
@@ -33,41 +33,39 @@ pub struct Field {
     argument: Option<String>,
 }
 
-fn parse_raw_symbols(lines: Vec<&str>) -> Vec<RawSymbol> {
-    let mut symbols = Vec::new();
-
-    for line in lines {
-        #[allow(clippy::len_zero)]
-        if line.len() == 0 {
-            continue;
-        }
-
-        let indent = line.chars().take_while(|x| *x == ' ').count();
-
-        let remaining = line.chars().skip(indent).collect::<String>();
-
-        let objects = remaining
-            .split_terminator(' ')
-            .into_iter()
-            .map(parse_identifier)
-            .collect::<Vec<_>>();
-
-        symbols.push(RawSymbol { indent, objects })
+fn parse_raw_symbol(raw: &str) -> Option<RawSymbol> {
+    if raw.is_empty() {
+        return None;
     }
 
-    symbols
+    let indent = raw.chars().take_while(|x| *x == ' ').count();
+    let remaining = raw.chars().skip(indent).collect::<String>();
+
+    let objects = remaining
+        .split_terminator(' ')
+        .into_iter()
+        .map(parse_identifier)
+        .collect::<Vec<_>>();
+
+    Some(RawSymbol { indent, objects })
+}
+
+fn parse_symbol_tree(lines: Vec<&str>) -> Vec<RawSymbol> {
+    lines
+        .into_iter()
+        .filter_map(parse_raw_symbol) // removes None
+        .collect()
 }
 
 pub fn parse() -> Node {
-    let symbols = std::fs::read_to_string("locals.symbols").unwrap();
+    let symbols = std::fs::read_to_string("programs/locals.symbols").unwrap();
 
     let ast = symbols.split(" == BOUND SYNTAX TREE == \n").last().unwrap();
     let ast = ast.split('\n').map(|x| x.trim_end()).collect::<Vec<&str>>();
 
-    let symbols = parse_raw_symbols(ast);
+    let mut symbols = parse_symbol_tree(ast);
 
     // add one 0-level at the end to pop everything
-    let mut symbols = symbols;
     symbols.push(RawSymbol {
         indent: 0,
         objects: vec![],
@@ -117,11 +115,10 @@ pub fn parse() -> Node {
 
 fn generate(syms: &Vec<RawSymbol>, graph: &Vec<Vec<usize>>, idx: usize) -> Node {
     // first generate any children
-    let mut children = Vec::new();
-    for ci in &graph[idx] {
-        let ch = generate(syms, graph, *ci);
-        children.push(ch);
-    }
+    let children = graph[idx]
+        .iter()
+        .map(|&i| generate(syms, graph, i))
+        .collect();
 
     // then generate this one
     generate_node_good(Entry(syms[idx].objects.clone()), &children)
