e7073ecedcdc6c0d26b6ca83c2191f134fa485b7
Jonas <jonas@jole.xyz>
2023-05-04 16:33:34 +0200

    write output to stdout

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,8 @@ mod ast;
 fn main() {
     let v = ast::parse();
 
-    let out = std::fs::File::create("out.s").unwrap();
-
-    let writer = BufWriter::new(out);
-    //let stdout = std::io::stdout();
-    //let stdout = BufWriter::new(stdout.lock());
+    let stdout = std::io::stdout();
+    let writer = BufWriter::new(stdout.lock());
 
     v.compile(writer);
 }
